033a9b7d699ad1311a01ef0c0f24406a9eb7ff94
Luis Gustavo <130862569+lgmartins0101@users.noreply.github.com>
2024-09-04 10:19:33 -0300

    fix livro e autor view
    Agora é possivel consultar os livros e utilizar put e delete
    
    Bem como criar, e manipular autores

--- core/urls.py
+++ core/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path("livros/", views.livro_list_create, name="livros-list-create"),
     path("livros/<int:pk>/", views.livro_detail, name="livro-detail"),
+    path("autores/", views.autor_list_create, name="autores-list-create"),
+    path("autores/<int:pk>/", views.autor_detail, name="autor-detail"),
 ]
--- core/views.py
+++ core/views.py
@@ -1,8 +1,8 @@
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.response import Response
 from rest_framework import status
-from .models import Livro
-from .serializers import LivroSerializer
+from .models import Livro, Autor
+from .serializers import LivroSerializer, AutorSerializer
 from rest_framework.decorators import api_view
 
 
@@ -22,6 +22,7 @@ def livro_list_create(request):
 
 
 @csrf_exempt
+@api_view(['GET', 'PUT', 'DELETE'])
 def livro_detail(request, pk):
     livro = Livro.objects.get(pk=pk)
     if request.method == "GET":
@@ -35,4 +36,39 @@ def livro_detail(request, pk):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     if request.method == "DELETE":
         livro.delete()
+    return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+@csrf_exempt
+@api_view(['GET', 'POST'])
+def autor_list_create(request):
+    if request.method == "GET":
+        autores = Autor.objects.all()
+        serializer = AutorSerializer(autores, many=True)
+        return Response(serializer.data)
+    if request.method == "POST":
+        serializer = AutorSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@csrf_exempt
+@api_view(['GET', 'PUT', 'DELETE'])
+def autor_detail(request, pk):
+    autor = Autor.objects.get(pk=pk)
+    if request.method == "GET":
+        serializer = AutorSerializer(autor)
+        return Response(serializer.data)
+    if request.method == "PUT":
+        serializer = AutorSerializer(autor, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    if request.method == "DELETE":
+        autor.delete()
+        return Response({"message":"Instancia apagada com sucesso"}, status=200)
+
     return Response(status=status.HTTP_204_NO_CONTENT)
