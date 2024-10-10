b409bd10e71e6122d39e0649a2705ba8bf94e550
Gikeun11 <154636079+Gikeun11@users.noreply.github.com>
2024-10-09 23:56:34 -0300

    Update views.py

--- core/views.py
+++ core/views.py
@@ -1,52 +1,23 @@
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.response import Response
 from rest_framework import status
-from .models import Livro, Autor
-from .serializers import LivroSerializer, AutorSerializer
 from rest_framework.decorators import api_view
 
 
-# @csrf_exempt
-# @api_view(['GET', 'POST'])
-# def livro_list_create(request):
-#     if request.method == "GET":
-#         livros = Livro.objects.all()
-#         serializer = LivroSerializer(livros, many=True)
-#         return Response(serializer.data)
-#     if request.method == "POST":
-#         serializer = LivroSerializer(data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data, status=status.HTTP_201_CREATED)
-#     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-
-# @csrf_exempt
-# @api_view(['GET', 'PUT', 'DELETE'])
-# def livro_detail(request, pk):
-#     livro = Livro.objects.get(pk=pk)
-#     if request.method == "GET":
-#         serializer = LivroSerializer(livro)
-#         return Response(serializer.data)
-#     if request.method == "PUT":
-#         serializer = LivroSerializer(livro, data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-#     if request.method == "DELETE":
-#         livro.delete()
-#     return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 from rest_framework import generics
 from .models import Livro, Autor, Categoria
 from .serializers import LivroSerializer, AutorSerializer, CategoriaSerializer
+from .filters import LivroFilter
 
 class LivroList(generics.ListCreateAPIView):
     queryset = Livro.objects.all()
     serializer_class = LivroSerializer
+    filterset_class = LivroFilter
     name = "livro-list"
+    
 
 class LivroDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Livro.objects.all()
