6f49e164b4ce4ac89d8227c2644c773e79f9fbc4
lgmartins0101 <lgmartins0101@gmail.com>
2024-10-09 23:51:52 -0300

    refatoracao classe views e urls

--- core/urls.py
+++ core/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("livros/", views.livro_list_create, name="livros-list-create"),
-    path("livros/<int:pk>/", views.livro_detail, name="livro-detail"),
+    path("livros/", views.LivroList.as_view(), name="livros-list-create"),
+    path("livros/<int:pk>/", views.LivroDetail.as_view(), name="livro-detail"),
     path("autores/", views.autor_list_create, name="autores-list-create"),
     path("autores/<int:pk>/", views.autor_detail, name="autor-detail"),
 ]
--- core/views.py
+++ core/views.py
@@ -6,71 +6,70 @@ from .serializers import LivroSerializer, AutorSerializer
 from rest_framework.decorators import api_view
 
 
-@csrf_exempt
-@api_view(['GET', 'POST'])
-def livro_list_create(request):
-    if request.method == "GET":
-        livros = Livro.objects.all()
-        serializer = LivroSerializer(livros, many=True)
-        return Response(serializer.data)
-    if request.method == "POST":
-        serializer = LivroSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+# @csrf_exempt
+# @api_view(['GET', 'POST'])
+# def livro_list_create(request):
+#     if request.method == "GET":
+#         livros = Livro.objects.all()
+#         serializer = LivroSerializer(livros, many=True)
+#         return Response(serializer.data)
+#     if request.method == "POST":
+#         serializer = LivroSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-@csrf_exempt
-@api_view(['GET', 'PUT', 'DELETE'])
-def livro_detail(request, pk):
-    livro = Livro.objects.get(pk=pk)
-    if request.method == "GET":
-        serializer = LivroSerializer(livro)
-        return Response(serializer.data)
-    if request.method == "PUT":
-        serializer = LivroSerializer(livro, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    if request.method == "DELETE":
-        livro.delete()
-        return Response({"message":"Instancia apagada com sucesso"}, status=200)
-
-    return Response(status=status.HTTP_204_NO_CONTENT)
+# @csrf_exempt
+# @api_view(['GET', 'PUT', 'DELETE'])
+# def livro_detail(request, pk):
+#     livro = Livro.objects.get(pk=pk)
+#     if request.method == "GET":
+#         serializer = LivroSerializer(livro)
+#         return Response(serializer.data)
+#     if request.method == "PUT":
+#         serializer = LivroSerializer(livro, data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#     if request.method == "DELETE":
+#         livro.delete()
+#     return Response(status=status.HTTP_204_NO_CONTENT)
 
 
-@csrf_exempt
-@api_view(['GET', 'POST'])
-def autor_list_create(request):
-    if request.method == "GET":
-        autores = Autor.objects.all()
-        serializer = AutorSerializer(autores, many=True)
-        return Response(serializer.data)
-    if request.method == "POST":
-        serializer = AutorSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+from rest_framework import generics
+from .models import Livro, Autor, Categoria
+from .serializers import LivroSerializer, AutorSerializer, CategoriaSerializer
+
+class LivroList(generics.ListCreateAPIView):
+    queryset = Livro.objects.all()
+    serializer_class = LivroSerializer
+    name = "livro-list"
+
+class LivroDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Livro.objects.all()
+    serializer_class = LivroSerializer
+    name = "livro-detail"
 
 
-@csrf_exempt
-@api_view(['GET', 'PUT', 'DELETE'])
-def autor_detail(request, pk):
-    autor = Autor.objects.get(pk=pk)
-    if request.method == "GET":
-        serializer = AutorSerializer(autor)
-        return Response(serializer.data)
-    if request.method == "PUT":
-        serializer = AutorSerializer(autor, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    if request.method == "DELETE":
-        autor.delete()
-        return Response({"message":"Instancia apagada com sucesso"}, status=200)
+class AutorList(generics.ListCreateAPIView):
+    queryset = Autor.objects.all()
+    serializer_class = AutorSerializer
+    name = "autor-list"
 
-    return Response(status=status.HTTP_204_NO_CONTENT)
+class AutorDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Autor.objects.all()
+    serializer_class = AutorSerializer
+    name = "autor-detail"
+
+class CategoriaList(generics.ListCreateAPIView):
+    queryset = Categoria.objects.all()
+    serializer_class = CategoriaSerializer
+    name = "categoria-list"
+
+class CategoriaDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Categoria.objects.all()
+    serializer_class = CategoriaSerializer
+    name = "categoria-detail"
