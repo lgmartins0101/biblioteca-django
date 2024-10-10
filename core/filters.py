dd899d73c717ecb8ab29ae7577eac74fcd7f5959
Gikeun11 <154636079+Gikeun11@users.noreply.github.com>
2024-10-09 23:57:35 -0300

    Create filters.py

---
+++ core/filters.py
@@ -0,0 +1,18 @@
+from django_filters import rest_framework as filters
+from .models import Livro, Categoria
+class LivroFilter(filters.FilterSet):
+    titulo = filters.CharFilter(lookup_expr='icontains')
+    autor = filters.CharFilter(field_name='autor__nome',lookup_expr='icontains')
+    categoria = filters.AllValuesFilter(field_name='categoria__nome')
+
+class Meta:
+    model = Livro
+    fields = ['titulo', 'autor', 'categoria']
+
+
+class CategoriaFilter(filters.FilterSet):
+    nome = filters.CharFilter(lookup_expr='startswith')  # Busca por nome que começa com a string
+
+    class Meta:
+        model = Categoria
+        fields = ['nome']
